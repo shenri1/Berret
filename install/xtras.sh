5464e576387745f3707fed93a8b4d2ea8b596550
Silas Cerqueira <silas10henriique@gmail.com>
2026-02-22 18:48:06 -0300

    Add OpenCode and Zed Text Editor

--- install/xtras.sh
+++ install/xtras.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+
+curl -fsSL https://opencode.ai/install | bash
+
+curl -f https://zed.dev/install.sh | sh
